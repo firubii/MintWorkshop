1c24243d612987bc030e0deaa83b3d98285704f7
firubii <fruitymage@gmail.com>
2021-06-09 23:17:07 -0500

    New flag label + one renamed

--- MintWorkshop/Mint/FlagLabels.cs
+++ MintWorkshop/Mint/FlagLabels.cs
@@ -11,7 +11,7 @@ namespace MintWorkshop.Mint
         public static Dictionary<uint, string> ClassFlags = new Dictionary<uint, string>()
         {
             {  0x2, "enum" },
-            {  0x4, "struct" },
+            {  0x4, "pod" },
             {  0x8, "util" },
         };
 
@@ -24,6 +24,7 @@ namespace MintWorkshop.Mint
         public static Dictionary<uint, string> FunctionFlags = new Dictionary<uint, string>()
         {
             {  0x1, "init" },
+            {  0x4, "public" },
             {  0x8, "null" },
             { 0x80, "return" },
         };
