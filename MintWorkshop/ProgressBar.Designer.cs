1c064ae9229e1cfc87101638e8cf6a3a48892123
firubii <fruitymage@gmail.com>
2022-01-16 16:46:24 -0600

    Progress Bar dialog for later

---
+++ MintWorkshop/ProgressBar.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace MintWorkshop
+{
+    partial class ProgressBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(0, 23);
+            this.progress.Maximum = 1;
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(411, 36);
+            this.progress.TabIndex = 0;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(0, 4);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 13);
+            this.status.TabIndex = 1;
+            // 
+            // ProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 59);
+            this.ControlBox = false;
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.progress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressBar";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label status;
+    }
+}
---
+++ MintWorkshop/ProgressBar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MintWorkshop
+{
+    public partial class ProgressBar : Form
+    {
+        string statusText = "";
+
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        public void SetMax(int max)
+        {
+            progress.Maximum = max;
+            UpdateDisplay();
+        }
+
+        public void SetValue(int value)
+        {
+            progress.Value = value;
+            UpdateDisplay();
+        }
+
+        public void IncrementValue()
+        {
+            progress.Value++;
+            UpdateDisplay();
+        }
+
+        public void SetTitle(string text)
+        {
+            this.Text = text;
+            UpdateDisplay();
+        }
+
+        public void SetStatus(string text)
+        {
+            statusText = text;
+            UpdateDisplay();
+        }
+
+        public void UpdateDisplay()
+        {
+            status.Text = $"{statusText} {progress.Value}/{progress.Maximum} - {progress.Value/progress.Maximum * 100f}%";
+        }
+
+    }
+}
