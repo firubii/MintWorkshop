83d8d3c0da9c5e32a8795bacb3a64a7a4d4d6e3a
firubii <fruitymage@gmail.com>
2025-10-13 16:38:38 -0500

    Properly update Archive nodes after using "Build As"

--- MintWorkshop/MainForm.cs
+++ MintWorkshop/MainForm.cs
@@ -1409,6 +1409,7 @@ namespace MintWorkshop
                 BuildArchive(ctx);
 
                 archives[arcTree.SelectedNode.Index] = ctx;
+                UpdateArchiveNodes();
             }
         }
 
